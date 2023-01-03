55f99c8ae51ce24d63936807e1315063fc6c107d
Cristian Zapata <c.zapata.iq@gmail.com>
2023-01-03 18:32:14 -0500

    categories

--- src/main_home.js
+++ src/main_home.js
@@ -1,10 +1,14 @@
-async function getTrendingMoviesPreview(){
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
-    const data = await res.json();
+async function getTrendingPreview(){
+    const res1 = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const res2 = await fetch('https://api.themoviedb.org/3/trending/tv/day?api_key=' + API_KEY);
+    const data1 = await res1.json();
+    const data2 = await res2.json();
 
-    const movies = data.results;
+    const movies = data1.results;
+    const series = data2.results;
     const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
-    
+    const trendingPreviewSeriesContainer = document.querySelector('#trendingPreview .trendingPreview-seriesList');
+
     movies.forEach(movie => {
 
         
@@ -24,6 +28,53 @@ async function getTrendingMoviesPreview(){
         trendingPreviewMoviesContainer.appendChild(movieContainer);
     });
 
+    series.forEach(serie => {
+
+        
+
+        const serieContainer = document.createElement('div')
+        serieContainer.classList.add('serie-container');
+    
+        const serieImg = document.createElement('img')
+        serieImg.classList.add('serie-img');
+        serieImg.setAttribute('alt', serie.title)
+        serieImg.setAttribute(
+        'src', 
+        'https://image.tmdb.org/t/p/w300/' + serie.poster_path,
+        );
+    
+        serieContainer.appendChild(serieImg);
+        trendingPreviewSeriesContainer.appendChild(serieContainer);
+    });
 }
 
-getTrendingMoviesPreview();
+async function getCategoriesPreview(){
+    const res1 = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data1 = await res1.json();
+
+    const genres = data1.genres;
+    const PreviewGenresContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+    console.log(genres)
+    genres.forEach(genre => {
+
+        const categoryContainer = document.createElement('div')
+        categoryContainer.classList.add('category-container');
+
+        const categoryTittle = document.createElement('button')
+        categoryTittle.classList.add('category-button');
+        categoryTittle.setAttribute('id', 'id' + genre.id);
+        
+        const categoryTittleText = document.createTextNode(genre.name);
+        
+        categoryTittle.appendChild(categoryTittleText);
+        categoryContainer.appendChild(categoryTittle);
+        PreviewGenresContainer.appendChild(categoryContainer);
+
+       
+    });
+
+}
+
+
+getTrendingPreview();
+getCategoriesPreview();
