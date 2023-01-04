8473d1cb5e63550461e442449c90ad6bd08cfa90
Cristian Zapata <c.zapata.iq@gmail.com>
2023-01-04 13:01:43 -0500

    axios added, navigation & nodes js

---
+++ src/navigation.js
@@ -0,0 +1,41 @@
+const ce = (element) => document.createElement(element);
+
+window.addEventListener('DOMContentLoaded', navigator(),false);
+window.addEventListener('hashchange', navigator(),false);
+
+function navigator() {
+    if(location.hash.startsWith('#trends')){
+        trendsPage();
+    }else if (location.hash.startsWith('#search=')){
+        searchPage();
+    }else if (location.hash.startsWith('#movie=')){
+        movieDetailsPage();
+    }else if (location.hash.startsWith('#category=')){
+        categoriesPage();
+    }else {
+        homePage();
+    }
+    
+}
+
+function homePage(){
+    console.log('Home');
+    getTrendingPreview();
+    getCategoriesPreview();   
+}
+
+function categoriesPage(){
+    console.log('Categories');
+}
+
+function movieDetailsPage(){
+    console.log('movie details');
+}
+
+function searchPage(){
+    console.log('Search page');
+}
+
+function trendsPage(){
+    console.log('Trends');
+}
